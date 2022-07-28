a42669103489458f15a21d9bc4fbd5cb3783cc74
Jeremiah Harriott <harriott_47@hotmail.com>
2022-07-28 20:08:43 +0100

    added timer stop when player or enemy wins match

--- index.js
+++ index.js
@@ -130,27 +130,32 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
     rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
    )
   }
-  let timer = 5
-  function decreasetimer() {
-    if (timer > 0) {
-      setTimeout(decreasetimer, 1000)
-      timer--
-      document.querySelector('#timer').innerHTML = timer
-    }
 
-    if (timer === 0) {
-      document.querySelector('#displayText').style.display = 'flex'
-
+  function determineWinner({ player, enemy, timerId }) {
+    clearTimeout(timerId)
+    document.querySelector('#displayText').style.display = 'flex'
     if (player.health === enemy.health) {
       document.querySelector('#displayText').innerHTML = 'Tie'
        } else if (player.health > enemy.health) {
         document.querySelector('#displayText').innerHTML = 'Player 1 Wins!'
       } else if (player.health < enemy.health) {
         document.querySelector('#displayText').innerHTML = 'Player 2 Wins!'
+  }
+}
+
+  let timer = 60
+  let timerId
+  function decreasetimer() {
+    if (timer > 0) {
+      timerId = setTimeout(decreasetimer, 1000)
+      timer--
+      document.querySelector('#timer').innerHTML = timer
+    }
+
+    if (timer === 0) {
+      determineWinner({player, enemy, timerId})
     }
   }
-
-    }
 
     decreasetimer()
 
@@ -200,8 +205,13 @@ function animate() {
     ) {
       enemy.isAttacking = false
       player.health -= 20
-       document.querySelector('#playerHealth').style.width = player.health + '%'
+      document.querySelector('#playerHealth').style.width = player.health + '%'
   }
+
+   // End of game based on health bar
+   if (enemy.health<= 0 || player.health <= 0) {
+      determineWinner({ player, enemy, timerId })
+   }
 }
 
 animate()
