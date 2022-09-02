6f215303790274d0876e3c95e7781046ecf37451
Jeremiah Harriott <harriott_47@hotmail.com>
2022-09-02 15:20:15 +0100

    both characters have detailed health bars and background opacity raised along with font change

--- index.js
+++ index.js
@@ -178,6 +178,8 @@ function animate() {
   c.fillRect(0, 0, canvas.width, canvas.height)
   background.update()
   shop.update()
+  c.fillStyle = 'rgba(255, 255, 255, .10)'
+  c.fillRect(0, 0, canvas.width, canvas.height)
   player.update()
   enemy.update()
 
@@ -230,7 +232,9 @@ function animate() {
       enemy.takeHit()
       player.isAttacking = false
 
-      document.querySelector('#enemyHealth').style.width = enemy.health + '%'
+      gsap.to('#enemyHealth', {
+        width: enemy.health + '%'
+      })
   }
 
   // If player misses attack //
@@ -249,7 +253,9 @@ function animate() {
     ) {
       player.takeHit()
       enemy.isAttacking = false
-      document.querySelector('#playerHealth').style.width = player.health + '%'
+      gsap.to('#playerHealth', {
+        width: player.health + '%'
+      })
   }
 
  // If Enemy misses attack //
